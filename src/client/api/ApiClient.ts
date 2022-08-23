aaa7886e7950dd61aa0d7d171962b8faca11b0f8
lovelygracearsolon <lovelygracearsolon@gmail.com>
2022-08-23 15:47:00 +0800

    Add authorizaztion header in api requests

--- src/client/api/ApiClient.ts
+++ src/client/api/ApiClient.ts
@@ -1,8 +1,31 @@
 import axios from 'axios';
 
 import config from '../configs/config.dev.json';
+import { auth } from '../services/FirebaseService';
 
 export const baseURL = `${config.API_URL}/api/`;
 const ApiClient = axios.create({ baseURL });
+
+ApiClient.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    throw error;
+  },
+);
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    if (config.headers === undefined) {
+      config.headers = {};
+    } else {
+      const token = await auth.currentUser?.getIdToken(true);
+      config.headers.authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    throw error;
+  },
+);
 
 export default ApiClient;
