7c612fd0ee3f4158e70ee7c4af6d492f36905496
lovelygracearsolon <lovelygracearsolon@gmail.com>
2022-08-23 15:53:32 +0800

    Add authentication middleware in api

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import AuthMiddleware from '../middlewares/AuthMiddleware';
 
 import TaskRoute from './TaskRoute';
 import UserRoute from './UserRoute';
 
 const router = Router();
+
+router.use(AuthMiddleware.authenticateUser);
 
 router.use('/users', UserRoute);
 router.use('/tasks', TaskRoute);
