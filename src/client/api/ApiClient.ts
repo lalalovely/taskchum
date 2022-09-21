3b13047d6ddeeeeeac69bcf0641d2c5f5b96e1da
lovelygracearsolon <lovelygracearsolon@gmail.com>
2022-09-21 16:03:41 +0800

    Show error dialog when api response returns an error

--- src/client/api/ApiClient.ts
+++ src/client/api/ApiClient.ts
@@ -1,31 +1,26 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 import config from '../configs/config.dev.json';
+import { showError } from '../contexts/AlertDialogContext';
 import { auth } from '../services/FirebaseService';
 
 export const baseURL = `${config.API_URL}/api/`;
 const ApiClient = axios.create({ baseURL });
 
-ApiClient.interceptors.response.use(
-  (res) => res,
-  (error) => {
-    throw error;
-  },
-);
+ApiClient.interceptors.response.use((res) => res, interceptErrorResponse);
 
-ApiClient.interceptors.request.use(
-  async (config) => {
-    if (config.headers === undefined) {
-      config.headers = {};
-    } else {
-      const token = await auth.currentUser?.getIdToken(true);
-      config.headers.authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    throw error;
-  },
-);
+ApiClient.interceptors.request.use(async (config) => {
+  if (config.headers === undefined) {
+    config.headers = {};
+  } else {
+    const token = await auth.currentUser?.getIdToken(true);
+    config.headers.authorization = `Bearer ${token}`;
+  }
+  return config;
+}, interceptErrorResponse);
+
+function interceptErrorResponse(error: AxiosError) {
+  showError(error.message, 'error');
+}
 
 export default ApiClient;
